bae2ca2e538310024610906c382c43cd9897d382
HuynhTai488 <htai8400@gmail.com>
2025-06-16 23:02:14 +0700

    doi port

--- app.py
+++ app.py
@@ -26,5 +26,7 @@ def index():
         return render_template('index.html', image_url = IMAGE_PATH)
 
 
-if __name__ == '__main__':
-    app.run(debug=False)
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=False)
+

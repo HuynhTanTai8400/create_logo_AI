6d2eb1b3ac2dc934b97349a65abe67fe21cc33de
HuynhTai488 <htai8400@gmail.com>
2025-06-17 15:44:16 +0700

    change model stable diffusion

--- create_logo.py
+++ create_logo.py
@@ -4,7 +4,7 @@ from PIL import Image
 
 # Mô hình và prompt
 # Sửa model_list thành biến đơn hoặc danh sách nếu có nhiều model
-model_name_sd = "runwayml/stable-diffusion-v1-5"
+model_name_sd = "CompVis/stable-diffusion-v1-4"
 
 
 # Dinh nghia tham so
